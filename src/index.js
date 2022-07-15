d23b67e7085ba81801c1e6b0eaa944130559df3f
D Dranidis <dranidis@gmail.com>
2022-07-15 18:18:43 +0300

    Format

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
-const express = require('express')
-const { info, start, move, end } = require('./logic')
+const express = require("express");
+const { info, start, move, end } = require("./logic");
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 app.use(function (req, res, next) {
-    res.set("Server", "BattlesnakeOfficial/starter-snake-javascript")
-    next()
-})
+  res.set("Server", "BattlesnakeOfficial/starter-snake-javascript");
+  next();
+});
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-    res.send(info())
+  res.send(info());
 });
 
 app.post("/start", (req, res) => {
-    res.send(start(req.body))
+  res.send(start(req.body));
 });
 
 app.post("/move", (req, res) => {
-    res.send(move(req.body))
+  res.send(move(req.body));
 });
 
 app.post("/end", (req, res) => {
-    res.send(end(req.body))
+  res.send(end(req.body));
 });
 
 // Start the Express server
 app.listen(port, () => {
-    console.log(`Starting Battlesnake Server at http://0.0.0.0:${port}...`)
-})
+  console.log(`Starting Battlesnake Server at http://0.0.0.0:${port}...`);
+});
